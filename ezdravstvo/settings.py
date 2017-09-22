5c2c28cd3280cd9151ee78a863551897be922813
VelikovPetar <petarvelikov95@gmail.com>
2017-09-22 13:21:39 +0200

    Added apps for Patient/Doctor

--- ezdravstvo/settings.py
+++ ezdravstvo/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'patient.apps.PatientConfig',
+    'doctor.apps.DoctorConfig',
     'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- ezdravstvo/urls.py
+++ ezdravstvo/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
 
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('login.urls', namespace="login"))
+    url(r'^', include('login.urls', namespace="login")),
+    url(r'^', include('patient.urls', namespace="patient")),
+    url(r'^', include('doctor.urls', namespace="doctor"))
 ]
