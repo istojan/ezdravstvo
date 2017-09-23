60ff37146e3f2c66387f21ea6df4fd61ee4e3278
VelikovPetar <petarvelikov95@gmail.com>
2017-09-23 14:21:00 +0200

    Patient peview

--- doctor/urls.py
+++ doctor/urls.py
@@ -6,6 +6,7 @@ app_name = 'doctor'
 urlpatterns = [
     url(r'^doctor/(?P<doctor_id>[0-9]+)/$', views.homepage, name='homepage_doc'),
     url(r'^doctor/(?P<doctor_id>[0-9]+)/makeappointment/$', views.MakeAppointmentView.as_view(), name='make_appointment'),
+    url(r'^doctor/(?P<doctor_id>[0-9]+)/patients$', views.PatientsPreviewView.as_view(), name='patients_preview')
     # url(r'^patient/(?P<patient_id>[0-9]+)/homepage/$', views.homepage, name='homepage'),
     # url(r'^patient/(?P<patient_id>[0-9]+)/oldAppointments/$', views.old_appointments, name='old_appointments'),
     # url(r'^patient/(?P<patient_id>[0-9]+)/upcomingAppointments', views.upcoming_appointments, name='upcoming_appointments'),
